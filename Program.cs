f0cf68753ca46459b4ea6bcb82f8bac3742da802
Sveta <vera.mak@yandex.ru>
2022-12-06 18:09:19 +0300

    [f[f

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿//#ВТОРАЯ ЗАДАЧА
+//  Console.Write("Введите первое число: ");
+//  int firstNumber = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("Введите второе число: ");
+//  int secondNumber = Convert.ToInt32(Console.ReadLine());
+//  int MaxNumber = 0;
+//  int MinNumber = 0;
+//  if (firstNumber > secondNumber)
+//  {
+//      Console.WriteLine(MaxNumber = firstNumber);
+//      Console.WriteLine(MinNumber = secondNumber);
+//  }
+//  else 
+//  if (secondNumber > firstNumber)
+//  {
+//      Console.WriteLine(MaxNumber = secondNumber);
+//      Console.WriteLine(MinNumber = firstNumber);
+//  }
+
+
+//#ЧЕТВЕРТАЯ ЗАДАЧА
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
+// int MaxNumber = firstNumber;
+// Console.Write("Максимальное число: ");
+//    if (secondNumber > MaxNumber)
+//    {
+//     Console.Write(MaxNumber = secondNumber);
+//    }
+//    if (thirdNumber >  MaxNumber)
+//    {
+//     Console.Write(MaxNumber = thirdNumber);
+//    }
+   
+
+   //#ШЕСТАЯ ЗАДАЧА
+//    int NumberA = new Random().Next(0, 1000);
+//    Console.WriteLine(NumberA);
+//    if (NumberA % 2 == 0) // Число четное
+//    {
+//     Console.Write(true);
+//    }
+//    else
+//    if (NumberA % 2 == 1) // Число нечетное
+//    {
+//     Console.Write(false);
+//    }
+
+
+   //#ВОСЬМАЯ ЗАДАЧА
+// int NumberA = new Random().Next(5, 20);
+// Console.WriteLine(NumberA);
+// int I = 0;
+// while (I < NumberA)
+// {
+//    I = I + 2;
+//    Console.WriteLine(I);
+// }
